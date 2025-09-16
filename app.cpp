b93ad85a7450dac03b26dae077a63acbd6562da2
jblanked <82678820+jblanked@users.noreply.github.com>
2025-09-15 20:00:32 -0400

    check connection/credentials

--- app.cpp
+++ app.cpp
@@ -109,6 +109,29 @@ void FlipperHTTPApp::callbackSubmenuChoices(uint32_t index)
     switch (index)
     {
     case FlipperHTTPSubmenuRun:
+
+        // if the board is not connected, we can't use WiFi
+        if (!isBoardConnected())
+        {
+            easy_flipper_dialog("FlipperHTTP Error", "Ensure your WiFi Developer\nBoard or Pico W is connected\nand the latest FlipperHTTP\nfirmware is installed.");
+            return;
+        }
+
+        // if we don't have WiFi credentials, we can't connect to WiFi in case
+        // we are not connected to WiFi yet
+        if (!hasWiFiCredentials())
+        {
+            easy_flipper_dialog("No WiFi Credentials", "Please set your WiFi SSID\nand Password in Settings.");
+            return;
+        }
+
+        // if we don't have user credentials, we can't connect to the user account
+        if (!hasUserCredentials())
+        {
+            easy_flipper_dialog("No User Credentials", "Please set your Username\nand Password in Settings.");
+            return;
+        }
+
         if (!run)
         {
             run = std::make_unique<FlipperHTTPRun>(this);
@@ -219,6 +242,26 @@ bool FlipperHTTPApp::httpRequestAsync(
     return true;
 }
 
+bool FlipperHTTPApp::hasWiFiCredentials()
+{
+    char ssid[64] = {0};
+    char password[64] = {0};
+    return loadChar("wifi_ssid", ssid, sizeof(ssid)) &&
+           loadChar("wifi_pass", password, sizeof(password)) &&
+           strlen(ssid) > 0 &&
+           strlen(password) > 0;
+}
+
+bool FlipperHTTPApp::hasUserCredentials()
+{
+    char username[64] = {0};
+    char password[64] = {0};
+    return loadChar("user_name", username, sizeof(username)) &&
+           loadChar("user_pass", password, sizeof(password)) &&
+           strlen(username) > 0 &&
+           strlen(password) > 0;
+}
+
 bool FlipperHTTPApp::isBoardConnected()
 {
     if (!flipperHttp)
@@ -233,7 +276,7 @@ bool FlipperHTTPApp::isBoardConnected()
         return false;
     }
 
-    furi_delay_ms(100);
+    furi_delay_ms(500);
 
     // Try to wait for pong response.
     uint32_t counter = 100;
